43c4d750f4ce630382438029f6ea602d22c1058f
Charles Santos <coliveirasantos1234@gmail.com>
2026-01-05 20:37:03 -0300

    feat: create loginDTO

---
+++ backend/TodoApp/TodoApp/DTOs/AuthDtos/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoApp.DTOs.AuthDtos
+{
+    public class LoginDTO
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
