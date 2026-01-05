f02f1e147027ca1e276a108da48cb9be18647faa
Charles Santos <coliveirasantos1234@gmail.com>
2026-01-05 12:18:10 -0300

    need to restart

--- backend/TodoApp/TodoApp/Program.cs
+++ backend/TodoApp/TodoApp/Program.cs
@@ -1,21 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
 
-app.MapGet("/todo", () => "Todo Api rodando");  
+app.MapGet("/todos", () => { })
+.WithOpenApi();
 
 app.Run();
